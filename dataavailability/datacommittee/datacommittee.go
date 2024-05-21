15e5a8379d6bb6d7b1a3c6f503371f1a6803ebc2
Madhawa Ranaweera <madhawa.hasintha@igreendata.com.au>
2024-05-21 12:37:41 +1000

    config updated

--- dataavailability/datacommittee/datacommittee.go
+++ dataavailability/datacommittee/datacommittee.go
@@ -194,6 +194,7 @@ func requestSignatureFromMember(ctx context.Context, signedSequence daTypes.Sign
 	// request
 	c := client.New(member.URL)
 	log.Infof("sending request to sign the sequence to %s at %s", member.Addr.Hex(), member.URL)
+	log.Infof("sending request to sign the sequence in vallidium node ====>", fireblocksFeatureEnabled)
 	signature, err := c.SignSequence(signedSequence, fireblocksFeatureEnabled)
 	if err != nil {
 		ch <- signatureMsg{
